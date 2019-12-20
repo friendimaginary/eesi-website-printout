0456c9323879d04d03694dd0427f4aba7cbd9ca3
Patryk Soika <patryk@superrenaissanceman.com>
2019-12-20 19:46:34 +0000

    render "final" Word DOCx

--- cleaner_script.R
+++ cleaner_script.R
@@ -17,7 +17,10 @@ website <-
   arrange() %>%
   filter(str_detect(filename, ":$") == FALSE) %>%
   filter(str_detect(filename, "www.eesi.psu.edu&https") == FALSE) %>%
-  filter(str_detect(filename, "robots.txt$") == FALSE)
+  filter(str_detect(filename, "robots.txt$") == FALSE) %>%
+  filter(str_detect(filename, "www.eesi.psu.edu/news/") == FALSE) %>%
+  filter(str_detect(filename, ".edu/people/people-eye-eesi-research/") == FALSE)
+
 
 # Inspect the front page with file.edit(). Figure out where the headers and
 # footers begin and end. If the headers and footers are consisten across pages,
@@ -81,4 +84,24 @@ timing <-
   })
 
 file.edit("masterdoc.md")
+
 timing
+
+# > timing # A tibble: 1 x 13
+#
+# expression                min median `itr/sec` mem_alloc `gc/sec` n_itr  n_gc
+#
+# <bch:expr>              <bch> <bch:>     <dbl> <bch:byt>    <dbl> <int> <dbl>
+#
+# 1 {fetch_site(websi...} 3.12m  3.12m   0.00535     122MB    0.112     1    21
+#
+# # … with 5 more variables: total_time <bch:tm>, result <list>, memory <list>,
+# time <list>,
+#
+# #   gc <list>
+#
+# >
+
+rmarkdown::render('/home/pgs115/Documents/psu_git_repos/eesi-website-printout/masterdoc_without_news_eye.md',  encoding = 'UTF-8', knit_root_dir = '~/Documents/psu_git_repos/eesi-website-printout')
+
+rmarkdown::pandoc_convert(input = "masterdoc_without_news_eye.md", output = "masterdoc_without_news_eye.docx")
